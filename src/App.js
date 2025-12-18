c5c4e36b4b9da0095aff03e85c780cf372aaf51a
JKP_TS <jakkaphat254646@gmail.com>
2025-12-18 11:58:49 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <table border="1">
-      <tr><th>Poduct</th><th>Pice</th></tr>
-      <tr><td>React</td><td>320</td></tr>
-      <tr><td>React Native</td><td>340</td></tr>
-    </table>
-  )
+  const divStyle = {
+    color: 'red',
+    backgroundColor: 'powderblue',
+    fontSize: 'larger',
+    padding:'3px',
+  }
+  return <div style={{color: 'blue'}}>Hello React</div>
 }
 
 export default App;
