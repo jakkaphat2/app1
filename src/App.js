05b97036599584f410caab291b7a6c07d2ff899b
JKP_TS <115744033+jakkaphat2@users.noreply.github.com>
2026-02-12 10:05:27 +0700

    งานrouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import { Content, Footer, Header } from './func-components';
-import Calendar from './class-components';
-import Button from './class-components';
-import {Calculator2} from './Calculator';
-import EventData2 from './even-data';
-import RefsArray from './refs-array'
-import MessageBox from './state-func.js'
+import Router2 from './router2';
 
 function App() {
-  return <MessageBox/>
+  return <Router2/>
 }
  
 export default App; 
