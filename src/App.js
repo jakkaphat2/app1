c451b02f7959c1047aff2733dbdf07990e727cf0
JKP_TS <115744033+jakkaphat2@users.noreply.github.com>
2026-02-19 11:46:12 +0700

    Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import logo from './logo.svg';
-import './App.css';
-import Router2 from './router2';
-import Router3 from './router3';
+import FormText from './form-text'
 
 export default function App() {
-  return <Router3/>
+    return <FormText/>
 }
