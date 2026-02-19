fa5dc305767ee4ebb0313d35f7214175f4f046fa
JKP_TS <115744033+jakkaphat2@users.noreply.github.com>
2026-02-19 10:24:43 +0700

    Routers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Router2 from './router2';
+import Router3 from './router3';
 
-function App() {
-  return <Router2/>
+export default function App() {
+  return <Router3/>
 }
- 
-export default App; 
